6963ccd6d89d912e7dd3ca9dae430648a21cf49e
meimoon1 <melevelyn69@gmail.com>
2021-10-19 16:45:20 -0300

    remove e busca

--- ATP26/Dados.java
+++ ATP26/Dados.java
@@ -9,21 +9,44 @@ public class Dados {
         this.dados = new Object[tamanho];
     }
 
-    public void Adicionar(Object obj){
+    public String adicionar(Object obj){
+        String mensagem ="Salvo com sucesso";
         if(posicao < dados.length){
-            dados[posicao]=obj;
+            dados[posicao] = obj;
             posicao++;
         }else{
-            Object[]dados2 = new Object[dados.length+5];
+            tamanho = tamanho +5;
+            Object[]dados2 = new Object[tamanho];
+
+            for(int i = 0; i < dados.length; i++){
+                dados2[i]= dados[i];
+            }
             dados = dados2;
         }
+        return mensagem;
     }
 
     public int lista(){
        return posicao+1;
     }
 
-    public void remover(Object obj){
-        
+    public String remover(Object obj){
+        String mensagem = "Removido com sucesso";
+
+        for(int i = 0; i < dados.length; i++){
+            if(dados[i].equals(obj)){
+                posicao--;     
+            }
+        }
+        return mensagem;
+    }
+
+    public boolean buscar(Object obj){
+        for(int i =0; i< posicao; i++){
+            if(dados[i].equals(obj)){
+                return true;
+            }
+        }
+        return false;
     }
 }
