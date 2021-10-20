677081416ebcf3ff5672c82aea1ac9e6f999466a
meimoon1 <melevelyn69@gmail.com>
2021-10-20 11:37:03 -0300

    correcoes

--- ATP26/Aluno.java
+++ ATP26/Aluno.java
@@ -4,15 +4,22 @@ public class Aluno extends Pessoa{
     public String turma;
     public String curso;
 
+    //só usa equals para string
+    //tem que comparar todos os dados da filha e da mãe
     @Override
     public boolean equals(Object obj) {
-      Aluno a = (Aluno)obj;
-      return(this.matricula == a.matricula &&  this.turma == a.turma && this.curso == a.curso);
+      if(obj instanceof Aluno){
+        Aluno outro = (Aluno)obj;
+        // esse super.equals() não precisa repetir todas as comparações da mãe
+        if(this.matricula.equals(outro.matricula)&& this.turma.equals(outro.turma) && this.curso.equals(outro.turma)&& super.equals(outro)){
+
+        }
+      }
+      return false;
     }
 
     @Override
-    public String toString() {
-        super.toString();
-        return "Matrícula: " + matricula + "\nTurma: " + turma + "\nCurso:" + curso + "\n";
+    public String toString() {   
+        return super.toString() + "Matrícula: " + matricula + "\nTurma: " + turma + "\nCurso:" + curso + "\n";
     }
 }
--- ATP26/Pessoa.java
+++ ATP26/Pessoa.java
@@ -1,16 +1,24 @@
+
 public class Pessoa{
     public String nome;
     public String sobrenome;
     public int idade;
 
+    //usado para compara o que é igual da classe
     @Override
     public boolean equals(Object obj) {
-        Pessoa p = (Pessoa)obj;
-        return (this.nome == p.nome && this.sobrenome == p.sobrenome && this.idade == p.idade);
+        //instance of pega qualquer variavel e verificar se foi criada como pessoa
+        if(obj instanceof Pessoa){
+            Pessoa outra = (Pessoa)obj;
+            if(this.nome.equals(outra.nome) && this.sobrenome.equals(outra.sobrenome) && this.idade == outra.idade){
+                return true;
+            }
+        }      
+        return false;
     }
 
     @Override
     public String toString() {
-        return "Nome: "+ nome + "\nSobrenome: " + sobrenome + "\nIdade: " + idade + "\n";
+        return "Nome: "+ this.nome + "\nSobrenome: " + this.sobrenome + "\nIdade: " + this.idade + "\n";
     }
 }
