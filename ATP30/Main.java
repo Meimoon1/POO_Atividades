03d055759d9e4ad559890c7d7da28f047c38a3ab
meimoon1 <melevelyn69@gmail.com>
2021-10-25 15:33:35 -0300

    finalizando

--- ATP30/Main.java
+++ ATP30/Main.java
@@ -1,43 +1,62 @@
 import java.util.Scanner;
 public class Main{
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+      
       Calculadora c = new Calculadora();
 
       int opcao = 0;
       do{
           menu();
           opcao = escolhaMenu(sc);
+          int num1 =0;
+          int num2 = 0;
+          double resultado =0;
           switch(opcao){
               case 1 :
-              //Implementar para ser geral essa mensagem pedindo numero
-                System.out.print("Informe um número:");
-                int num1 = Integer.parseInt(sc.nextLine());
-                System.out.print("Informe um número:");
-                int num2 = Integer.parseInt(sc.nextLine());
-                int resultado = c.somar(num1, num2);           
-                System.out.println("A soma é:"+resultado);      
+                num1 = lerNumero("Informe um número:");
+                num2 = lerNumero("Informe outro número:");
+                resultado = c.somar(num1, num2);
+                System.out.println("A soma é:"+resultado);
               break;
 
               case 2:
+                num1 = lerNumero("Informe um número:");
+                num2 = lerNumero("Informe outro número:");
+                resultado = c.subtrair(num1, num2);
+                System.out.println("A subtração é:"+resultado);
+                
               break;
 
               case 3:
+                num1 = lerNumero("Informe um número:");
+                num2 = lerNumero("Informe outro número:");
+                resultado = c.multiplicar(num1, num2);
+                System.out.println("A multiplicação é:"+resultado);
               break;
 
               case 4:
-                try {
-                   c.dividir(3, 1);
-                } catch (Exception e) {
-                    //TODO: handle exception
-                }
+                num1 = lerNumero("Informe um número:");
+                boolean validacao = true;
+              do {
+                    try {
+                        num2 = lerNumero("Informe o outro número");
+                        evitaZero(num2);
+                    } catch (RuntimeException ex) {
+                        System.out.println(ex.getMessage()+"Digite novamente");
+                        validacao = false;
+                    }
+              } while (!validacao);
+              resultado = c.dividir(num1, num2);
+              System.out.println("A divisão é:"+resultado);  
               break;
 
               default:
                 System.out.println("Opção inválida");
-              break;
-          }
-      }while (opcao != 0);
+              break; 
+
+            }
+        }while (opcao != 0);
     }
    
 
@@ -47,8 +66,24 @@ public class Main{
     }    
 
     public static int escolhaMenu(Scanner sc){
-        System.out.print("Escolha uma opção: ");
-        int opcao = Integer.parseInt(sc.nextLine());
+        int opcao = 0;
+        do{
+            try {
+                System.out.print("Digite a opção:");
+                opcao = Integer.parseInt(sc.nextLine());
+
+                if(opcao<0 || opcao>=5){
+                    throw new IllegalArgumentException("Opção inválida\n");
+                }
+            } 
+            catch (NumberFormatException ex) {
+                System.out.println(ex.getMessage() + "Digite novamente!");
+            }
+            catch(IllegalArgumentException ex){
+                System.out.println(ex.getMessage()+ "Digite novamente!");
+            }
+
+        }while(opcao <=-1 || opcao>=5);
         return opcao;
     }
 
@@ -61,7 +96,7 @@ public class Main{
             try {
                 numero = Integer.parseInt(sc.nextLine());
             } catch (NumberFormatException ex) {
-                System.out.println("Número inválido. Digite Novamente!");
+                System.out.println("Número inválido. \nDigite Novamente!");
                 validacao = false;
             }
             
@@ -70,6 +105,11 @@ public class Main{
         
     }
 
-    //comentar bloco ctrl+k+c
-    //descomentar ctrl+u
+    static void evitaZero(int numero){
+        if(numero == 0){
+            throw new RuntimeException("Número Inválido");
+        }
+    }
+
+  
 }
