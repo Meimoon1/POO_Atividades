d73f09e6c345cfc1a3c9de6ad2156d4373ff28ad
meimoon1 <melevelyn69@gmail.com>
2021-11-04 09:43:20 -0300

    leitura atp39

---
+++ ATP39/Leitura.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Leitura{
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("dados/nomes.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            while(br.ready()){
+                String linha = br.readLine();
+                System.out.println(linha);
+            }
+            br.close();
+            } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+            }catch(IOException e){
+                System.out.println("Arquivo não encontrado");
+            }
+    }
+}
