e2cdefb9b3980067052fc3480af45a3f41a91fd6
meimoon1 <melevelyn69@gmail.com>
2021-11-08 11:12:54 -0300

    atp

--- ATP40/ex2/PontosTuristicos.java
+++ ATP40/ex2/PontosTuristicos.java
@@ -3,4 +3,9 @@ public class PontosTuristicos{
     public String nome;
     public String descricao;
     public String localizacao;
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + "Descrição: " + this.descricao + "Localização: ";
+    }
 }
