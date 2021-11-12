f3add5c3cc5be299cd669a4366dc5fc43f0250b5
meimoon1 <melevelyn69@gmail.com>
2021-11-12 00:08:28 -0300

    classe1 atp47

---
+++ ATP47/Classe1.java
@@ -0,0 +1,26 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Classe1{
+    public static void main(String[] args) {
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+            Statement statement = cn.createStatement();
+            String sql = "SELECT * FROM categoria";
+            statement.execute(sql);
+            ResultSet resultado = statement.getResultSet();
+
+            while(resultado.next()){
+                int id = resultado.getInt("id");
+                String nome = resultado.getString("nome");
+                String descricao = resultado.getString("descricao");
+                System.out.println("Id:"+id+" Nome:"+ nome+" Descricao:"+descricao+"\n");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Não foi possível acessar o arquivo");
+        }
+    }
+}
