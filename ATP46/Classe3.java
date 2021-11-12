735a3f444a21a53af9fd387b86e84ca006f19996
meimoon1 <melevelyn69@gmail.com>
2021-11-11 23:56:58 -0300

    atp46

---
+++ ATP46/Classe3.java
@@ -0,0 +1,3 @@
+public class Classe3 {
+    // falta consertar id_categoria no banco de dados para fazer.
+}
---
+++ ATP46/Classe4.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Classe4 {
+    public static void main(String[] args) {
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+     
+            Statement statement = cn.createStatement();
+            String sql = "DELETE FROM pessoa where id<3";
+            statement.execute(sql);
+            int linhasAfetadas = statement.getUpdateCount();
+            System.out.println("O número de linhas afetas foi:"+linhasAfetadas);
+           
+            cn.close();
+        } catch (SQLException e) {
+            System.out.println("Não foi possível conectar");
+        }
+    }
+}
