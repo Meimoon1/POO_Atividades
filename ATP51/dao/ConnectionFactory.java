e251d614be25e5f3363d3bfe6d3d4600963995e1
meimoon1 <melevelyn69@gmail.com>
2021-11-22 10:29:39 -0300

    factoru e dao

---
+++ ATP51/dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+    private DataSource dataSource;
+
+    public ConnectionFactory(){
+        ComboPooledDataSource poolConex = new ComboPooledDataSource();
+        poolConex.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        poolConex.setUser("postgres");
+        poolConex.setPassword("123456");
+        poolConex.setMaxPoolSize(50);
+        this.dataSource = poolConex;
+    }
+
+    public Connection getConnection() throws SQLException{
+        return this.dataSource.getConnection();
+    }
+    
+}
