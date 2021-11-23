f73f767aecf15fbadc4096108b33adcc32bcd845
meimoon1 <melevelyn69@gmail.com>
2021-11-23 10:09:07 -0300

    atp53

---
+++ ATP53/vendas/src/main/java/Categoria.java
@@ -0,0 +1,9 @@
+package src.main.java;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/categoria")
+public class Categoria extends HttpServlet{
+    
+    
+}
---
+++ ATP53/vendas/src/main/java/Produto.java
@@ -0,0 +1,7 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/produto")
+public class Produto extends HttpServlet{
+    
+}
