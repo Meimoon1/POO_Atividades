491bc5c4fd72dd7ee2451b13a5178880b46b559b
meimoon1 <melevelyn69@gmail.com>
2021-11-26 16:04:17 -0300

    atp60

---
+++ ATP60/banco/src/main/java/com/melissa/App.java
@@ -0,0 +1,13 @@
+package com.melissa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
